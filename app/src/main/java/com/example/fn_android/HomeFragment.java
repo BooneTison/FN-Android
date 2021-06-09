5b85bcc01d0f7d89ba10a2c2b576d59ef14d0e70
Boone Tison <boone.tison@furman.edu>
2021-06-09 15:08:45 -0400

    Updated home icons with better quality and added image at top of home screen

--- app/src/main/java/com/example/fn_android/HomeFragment.java
+++ app/src/main/java/com/example/fn_android/HomeFragment.java
@@ -33,47 +33,47 @@ public class HomeFragment extends Fragment {
 
         view.findViewById(R.id.athletics_button).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_athleticsFragment));
-        view.findViewById(R.id.athletics_btext).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
+        view.findViewById(R.id.athletics_button_text).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_athleticsFragment));
 
         view.findViewById(R.id.events_button).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_EventsFragment));
-        view.findViewById(R.id.events_btext).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
+        view.findViewById(R.id.events_button_text).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_EventsFragment));
 
         view.findViewById(R.id.dining_button).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_diningFragment));
-        view.findViewById(R.id.dining_btext).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
+        view.findViewById(R.id.dining_button_text).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_diningFragment));
 
         view.findViewById(R.id.health_safety_button).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_healthSafetyFragment));
-        view.findViewById(R.id.health_safety_btext).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
+        view.findViewById(R.id.health_safety_button_text).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_healthSafetyFragment));
 
         view.findViewById(R.id.phone_button).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_phoneFragment));
-        view.findViewById(R.id.phone_btext).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
+        view.findViewById(R.id.phone_button_text).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_phoneFragment));
 
         view.findViewById(R.id.transportation_button).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_transportationFragment));
-        view.findViewById(R.id.transportation_btext).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
+        view.findViewById(R.id.transportation_button_text).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_transportationFragment));
 
         view.findViewById(R.id.hours_button).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_hoursFragment));
-        view.findViewById(R.id.hours_btext).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
+        view.findViewById(R.id.hours_button_text).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_hoursFragment));
 
         view.findViewById(R.id.dates_button).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_datesFragment));
-        view.findViewById(R.id.dates_btext).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
+        view.findViewById(R.id.dates_button_text).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_datesFragment));
 
         view.findViewById(R.id.map_button).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_mapFragment));
-        view.findViewById(R.id.map_btext).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
+        view.findViewById(R.id.map_button_text).setOnClickListener(v -> NavHostFragment.findNavController(HomeFragment.this)
                 .navigate(R.id.action_homeFragment_to_mapFragment));
     }
 }
